162b2d69a56cba6b4954e9321df17541b77aa030
Maribel Montejano <outdoorsole@users.noreply.github.com>
2019-04-28 20:46:27 -0700

    Create String and Integer variables

--- Days1-12_Introduction_to_Swift/Day1_Variables_DataTypes_StringInterpolation/Variables.playground/Contents.swift
+++ Days1-12_Introduction_to_Swift/Day1_Variables_DataTypes_StringInterpolation/Variables.playground/Contents.swift
@@ -1,7 +1,18 @@
 import Cocoa
 
+// Type safety: each variable must be of one specific type
+
+
 // variables: can store program data, and can vary and values can change freely
+// str is of String type
 var str = "Hello, playground"
 
 // change the content of the variable
 str = "Goodbye"
+
+// age is of Int type
+var age = 38
+
+// underscores as thousands separators
+// will not change the number, but makes it easier to read
+var population = 8_000_000
