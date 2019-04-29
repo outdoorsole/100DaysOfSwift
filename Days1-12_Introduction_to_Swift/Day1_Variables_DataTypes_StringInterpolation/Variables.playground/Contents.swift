37e704a744ddcfaa462ccc8ae5a08959fd68fa4a
Maribel Montejano <outdoorsole@users.noreply.github.com>
2019-04-28 20:41:22 -0700

    Create and change content of the variable

--- Days1-12_Introduction_to_Swift/Day1_Variables_DataTypes_StringInterpolation/Variables.playground/Contents.swift
+++ Days1-12_Introduction_to_Swift/Day1_Variables_DataTypes_StringInterpolation/Variables.playground/Contents.swift
@@ -1,3 +1,7 @@
 import Cocoa
 
+// variables: can store program data, and can vary and values can change freely
 var str = "Hello, playground"
+
+// change the content of the variable
+str = "Goodbye"
