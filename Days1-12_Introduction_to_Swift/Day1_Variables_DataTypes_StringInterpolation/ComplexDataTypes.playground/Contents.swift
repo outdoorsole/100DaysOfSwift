c45effa5e29328f74afdc1d7e32e31691cf392d5
Maribel Montejano <outdoorsole@users.noreply.github.com>
2019-05-02 20:26:10 -0700

    Create an array of beatles

---
+++ Days1-12_Introduction_to_Swift/Day1_Variables_DataTypes_StringInterpolation/ComplexDataTypes.playground/Contents.swift
@@ -0,0 +1,11 @@
+import Cocoa
+
+let john = "John Lennon"
+let paul = "Paul McCartney"
+let george = "George Harrison"
+let ringo = "Ringo Starr"
+
+let beatles = [john, paul, george, ringo]
+
+beatles[1]
+
