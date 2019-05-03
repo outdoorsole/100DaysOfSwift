9129967c8f822453efb917ed0eca891a1773113d
Maribel Montejano <outdoorsole@users.noreply.github.com>
2019-05-03 12:36:58 -0700

    Use repeat loop to check condition

--- Days1-12_Introduction_to_Swift/Day1_Variables_DataTypes_StringInterpolation/Loops.playground/Contents.swift
+++ Days1-12_Introduction_to_Swift/Day1_Variables_DataTypes_StringInterpolation/Loops.playground/Contents.swift
@@ -26,6 +26,7 @@ for _ in 1...5 {
     print("play")
 }
 
+/*
 // WHILE LOOPS
 var number = 1
 
@@ -37,3 +38,27 @@ while number <= 20 {
 }
 
 print("Ready or not, here I come!")
+*/
+
+// REPEAT LOOPS
+// same as while loop, except condition check is at the end
+// the condition will be executed at least once, and then checked
+// (while loops check condition before they are run)
+var number = 1
+
+repeat {
+    print(number)
+    number += 1
+} while number <= 20
+
+print("Ready or not, here I come!")
+
+// this function will never be run
+while false {
+    print("This is false")
+}
+
+// Will run once, and then stop (after it fails on the check)
+repeat {
+    print("This is false")
+} while false
