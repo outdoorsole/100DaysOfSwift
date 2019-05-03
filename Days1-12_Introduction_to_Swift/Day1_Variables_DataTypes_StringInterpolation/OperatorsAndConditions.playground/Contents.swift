f0788343cabf7a04d955c433ffb5caf9b6310ecc
Maribel Montejano <outdoorsole@users.noreply.github.com>
2019-05-03 11:31:43 -0700

    Use arithmetic operators to perform calculations

---
+++ Days1-12_Introduction_to_Swift/Day1_Variables_DataTypes_StringInterpolation/OperatorsAndConditions.playground/Contents.swift
@@ -0,0 +1,19 @@
+import Cocoa
+
+let firstScore = 12
+let secondScore = 4
+
+// 12 + 4 = 16
+let total = firstScore + secondScore
+
+// 12 - 4 = 8
+let difference = firstScore - secondScore
+
+// 12 * 4 = 48
+let product = firstScore * secondScore
+
+// 12 / 4 = 3
+let divided = firstScore / secondScore
+
+// 13 % 4 = remainder 1
+let remainder = 13 % secondScore
