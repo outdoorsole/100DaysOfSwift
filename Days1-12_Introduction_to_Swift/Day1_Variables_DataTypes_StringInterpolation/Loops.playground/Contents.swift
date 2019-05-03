1ddadd4db5a32d8a69d421398095c7fa3c7aba8c
Maribel Montejano <outdoorsole@users.noreply.github.com>
2019-05-03 12:29:07 -0700

    Create for loops for ranges and arrays

---
+++ Days1-12_Introduction_to_Swift/Day1_Variables_DataTypes_StringInterpolation/Loops.playground/Contents.swift
@@ -0,0 +1,27 @@
+import Cocoa
+
+// FOR LOOPS
+// over ranges or arrays
+// for each loop iteration, it will pull one item out and assign it to a constant
+
+// from 1 to 10 (includes 10)
+let count = 1...10
+
+// iterate through range
+for number in count {
+    print("Number is \(number)")
+}
+
+let albums = ["Red", "1989", "Reputation"]
+
+// iterate through array
+for album in albums {
+    print("\(album) is on Apple Music")
+}
+
+print("Players gonna ")
+
+// if not using the constant, can use an underscore instead so that new constant (value) is not created
+for _ in 1...5 {
+    print("play")
+}
