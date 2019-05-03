1ee0dbbe8ed17bc4b76d93d71de2e9c851255efb
Maribel Montejano <outdoorsole@users.noreply.github.com>
2019-05-02 20:38:25 -0700

    Create a set of colors

--- Days1-12_Introduction_to_Swift/Day1_Variables_DataTypes_StringInterpolation/ComplexDataTypes.playground/Contents.swift
+++ Days1-12_Introduction_to_Swift/Day1_Variables_DataTypes_StringInterpolation/ComplexDataTypes.playground/Contents.swift
@@ -9,3 +9,11 @@ let beatles = [john, paul, george, ringo]
 
 beatles[1]
 
+// Set
+// Items are not in order (random order)
+// No item can appear twice in a set (must be unique)
+let colors = Set(["red", "green", "blue"])
+
+// duplicates are ignored
+// red, green, blue only
+let colors2 = Set(["red", "green", "blue", "red", "green"])
